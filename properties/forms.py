c9284082e0ca79c7e534abc3f86c057e040ea93d
aa-nadim <nadim.ice.nstu@gmail.com>
2024-12-02 01:30:56 +0600

    add signup form

--- properties/forms.py
+++ properties/forms.py
@@ -1,15 +1,29 @@
 from django import forms
 from .models import LocalizeAccommodation
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
-class LocalizeAccommodationForm(forms.ModelForm):
     class Meta:
-        model = LocalizeAccommodation
-        fields = ['accommodation', 'language', 'description', 'policy']
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
 
-    def clean_policy(self):
-        policy = self.cleaned_data.get('policy', {})
-        if not isinstance(policy, dict):
-            raise ValidationError("Policy must be a valid JSON dictionary.")
-        return policy
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError("Passwords do not match")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- properties/urls.py
+++ properties/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views
-from .views import my_properties, update_accommodation
 
 urlpatterns = [
     path('', views.home, name='home'), 
-    path('accommodation/<int:accommodation_id>/<str:language_code>/', 
-         views.localized_accommodation_view, name='localized_accommodation'),
-
-    path('my-properties/', my_properties, name='my_properties'),
-    path('accommodation/<int:pk>/update/', update_accommodation, name='update_accommodation'),
+    path('signup/', views.signup, name='signup'),
+    path('signup/success/', views.signup_success, name='signup_success'),
 ]
